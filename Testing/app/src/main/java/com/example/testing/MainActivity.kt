ed452d1138cac885b1c5ee1afe07a73383afaea7
EunhoKang <1999paul@naver.com>
2021-05-05 18:28:22 +0900

    210505 layout finished

---
+++ Testing/app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.testing
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.testing.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentByTag("navHostFragment")
+        if (navHostFragment != null) {
+            navController = navHostFragment.findNavController()
+        }
+    }
+}
